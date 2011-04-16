2ff908aa2833276a442252d9c6cf8ffb087ddf65
Alessandro Vendruscolo <alessandro.vendruscolo@gmail.com>
2011-04-16 19:45:56 +0200

    Show map with logo, general fixes

---
+++ js/initialize.js
@@ -0,0 +1,25 @@
+function initialize() {
+	var myLatlng = new google.maps.LatLng(45.51571, 9.21143);
+	var myOptions = {
+		zoom: 16,
+		center: myLatlng,
+		navigationControl: true,
+		navigationControlOptions: {
+			style: google.maps.NavigationControlStyle.SMALL
+		},
+		mapTypeControl: false,
+		scrollwheel: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	
+	var map = new google.maps.Map(document.getElementById("map"), myOptions);
+	
+	var image = new google.maps.MarkerImage('images/ruby_small.png');
+	
+	var marker = new google.maps.Marker({
+		position: myLatlng,
+		map: map,
+		animation: google.maps.Animation.DROP,
+		icon: image
+	});
+}
